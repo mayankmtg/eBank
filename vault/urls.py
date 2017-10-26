86921f02e3f3dd9dc5d55652ea6557526d6b8096
mayankmtg <mayank15056@iiitd.ac.in>
2017-10-27 00:31:34 +0530

    authentication debug and manager

--- vault/urls.py
+++ vault/urls.py
@@ -5,14 +5,17 @@ from . import views
 
 urlpatterns = [
 	# url(r'^login$', views.vaultLogin, name='vaultLogin'),
-	url(r'^$', views.vaultHome, name='vaultHome'),
-	url(r'^accounts/(?P<account_no_pk>\d+)/$', views.accountInfo, name='accountInfo'),
-	url(r'^accounts/(?P<account_no_pk>\d+)/transferfunds/$', views.transferfunds, name='transferfunds'),
-	url(r'login$', login, {'template_name':'accounts/login.html'}),
+	url(r'^$', views.vaultExternal, name='vaultExternal'),
+	url(r'^home/$', views.vaultHome, name='vaultHome'),
+	url(r'login/$', login, {'template_name':'accounts/login.html'}),
 	url(r'login_success/$', views.login_success, name='login_success'),
 	url(r'logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
+	url(r'^accounts/(?P<account_no_pk>\d+)/$', views.accountInfo, name='accountInfo'),
+	url(r'^accounts/(?P<account_no_pk>\d+)/transferfunds/$', views.transferfunds, name='transferfunds'),
+	url(r'^accounts/(?P<account_no_pk>\d+)/debit/$', views.vaultDebit, name='vaultDebit'),
+	url(r'^accounts/(?P<account_no_pk>\d+)/credit/$', views.vaultCredit, name='vaultCredit'),
 	url(r'^internal/$', views.vaultInternal, name='vaultInternal'),
-	url(r'^internal/debit/$', views.vaultDebit, name='vaultDebit'),
-	url(r'^internal/credit/$', views.vaultCredit, name='vaultCredit'),
+	url(r'^manager/$', views.vaultManager, name='vaultManager'),
+	url(r'^manager/(?P<transaction_pk>\d+)/approve/$', views.vaultTransactionApprove, name='vaultTransactionApproveManager'),
 	url(r'^internal/(?P<transaction_pk>\d+)/approve/$', views.vaultTransactionApprove, name='vaultTransactionApprove'),
-]
+]	
--- vault/views.py
+++ vault/views.py
@@ -7,8 +7,16 @@ from .models import user_account, cust_transaction
 
 
 
-@login_required(login_url='/login')
 def vaultHome(request):
+	context={}
+	return render(request, 'vault/home.html', context)
+
+@login_required(login_url='/login')
+def vaultExternal(request):
+	group=login_success(request)
+	if not (request.user.groups.filter(name="Ind_user").exists() or request.user.groups.filter(name="Organization").exists()):
+		# print(request.user.groups.filter(name="Ind_user").exists())
+		return group
 	context={
 		'accounts':user_account.objects.filter(cust_user_id=request.user),
 	}
@@ -17,21 +25,42 @@ def vaultHome(request):
 
 @login_required(login_url='/login')
 def vaultInternal(request):
+	group=login_success(request)
+	if request.user.groups.filter(name="Ind_user").exists() or request.user.groups.filter(name="Organization").exists() or request.user.groups.filter(name="Manager").exists():
+		return group
+
 	context={
-		'cust_transaction':cust_transaction.objects.filter(Amount__lte=100000, pending=True)
+		'cust_transaction':cust_transaction.objects.filter(Amount__lte=100000, pending=True) & cust_transaction.objects.filter(Amount__gte=-100000, pending=True),
+	}
+	return render(request, 'vault/internal.html', context)
+
+@login_required(login_url='/login')
+def vaultManager(request):
+	group=login_success(request)
+	if request.user.groups.filter(name="Ind_user").exists() or request.user.groups.filter(name="Organization").exists() or request.user.groups.filter(name="Regular").exists():
+		return group
+
+	context={
+		'cust_transaction':cust_transaction.objects.filter(Amount__gt=100000, pending=True) | cust_transaction.objects.filter(Amount__lt=-100000, pending=True),
 	}
 	return render(request, 'vault/internal.html', context)
 
 
 @login_required(login_url='/login')
 def accountInfo(request, account_no_pk):
+	cust_user=user_account.objects.filter(pk=account_no_pk)[0]
+	if request.user!=cust_user.cust_user_id:
+		return login_success(request)
 	context={
-		'account':user_account.objects.filter(pk=account_no_pk)[0],
+		'account':cust_user,
 	}
 	return render(request, 'vault/user_account.html', context)
 
 @login_required(login_url='/login')
 def transferfunds(request, account_no_pk):
+	cust_user=user_account.objects.filter(pk=account_no_pk)[0]
+	if request.user!=cust_user.cust_user_id:
+		return login_success(request)
 	if request.method=='POST':
 		amount=request.POST['amount']
 
@@ -41,13 +70,21 @@ def transferfunds(request, account_no_pk):
 		print(account_from, account_to)
 		t=cust_transaction(from_account=account_from, to_account=account_to, Amount=int(amount))
 		t.save()
-
+		return redirect('vault:accountInfo',account_no_pk)
 	context={
 		'account':user_account.objects.filter(pk=account_no_pk)[0],
 	}
 	return render(request, 'vault/user_transfer.html', context)
+@login_required(login_url='/login')
 def vaultTransactionApprove(request, transaction_pk):
+	group=login_success(request)
+	if not (request.user.groups.filter(name="Regular").exists() or request.user.groups.filter(name="Manager").exists()):
+		# print(request.user.groups.filter(name="Ind_user").exists())
+		return group
+
 	transaction=get_object_or_404(cust_transaction, pk=transaction_pk)
+	if(request.user.groups.filter(name="Regular").exists() and abs(transaction.Amount)>10000) or (request.user.groups.filter(name="Manager").exists() and abs(transaction.Amount)<=100000):
+		return group
 	if(transaction.pending==True):
 		from_account=transaction.from_account
 		to_account=transaction.to_account
@@ -61,7 +98,7 @@ def vaultTransactionApprove(request, transaction_pk):
 			transaction.pending=False
 			transaction.save()
 			return redirect('vault:vaultInternal')
-		elif to_account==from_account and from_account.cust_balance>=amount:
+		elif to_account==from_account and from_account.cust_balance+amount>=0:
 			from_account.cust_balance=from_account.cust_balance+amount
 			from_account.save()
 			transaction.pending=False
@@ -72,56 +109,59 @@ def vaultTransactionApprove(request, transaction_pk):
 	else:
 		return HttpResponse("Transaction Tackled")
 
-def vaultDebit(request):
-	
+@login_required(login_url='/login')
+def vaultDebit(request, account_no_pk):
+	cust_user=user_account.objects.filter(pk=account_no_pk)[0]
+	if request.user!=cust_user.cust_user_id:
+		return login_success(request)
 	context={}
 	if request.method=='POST':
 		amount=int(request.POST['amount'])
-		account_no=request.POST['acno']
 		if amount>0:
 			amount=-1*amount
-			account_from=user_account.objects.filter(pk=account_no)
+			account_from=user_account.objects.filter(pk=account_no_pk)[0]
 			account_to=account_from
 			t=cust_transaction(from_account=account_from, to_account=account_to, Amount=int(amount))
 			t.save()			
+			return redirect('vault:accountInfo',account_no_pk)
 
 		else:
 			HttpResponse("Invalid Amount")
-		# amount=request.POST['amount']
-		# account_no=int(request.POST['acno'])
-		# amount=abs(int(amount))
-		# account=get_object_or_404(user_account, pk=account_no)
-		# balance=account.cust_balance
-		# if balance >= amount:
-		# 	account.cust_balance=balance-amount
-		# 	account.save()
-		# 	return redirect('vault:vaultDebit')
-		# else:
-		# 	return HttpResponse("insufficient funds")
 
 	if request.method=='GET':
 		return render(request, 'vault/debit.html', context)
 
-def vaultCredit(request):
+@login_required(login_url='/login')
+def vaultCredit(request, account_no_pk):
+	cust_user=user_account.objects.filter(pk=account_no_pk)[0]
+	if request.user!=cust_user.cust_user_id:
+		return login_success(request)
 	context={}
 	if request.method=='POST':
 		amount=int(request.POST['amount'])
-		account_no=request.POST['acno']
 		if amount>0:
-			account_from=user_account.objects.filter(pk=account_no)[0]
+			account_from=user_account.objects.filter(pk=account_no_pk)[0]
 			account_to=account_from
 			t=cust_transaction(from_account=account_from, to_account=account_to, Amount=int(amount))
 			t.save()			
-			return redirect()
+			return redirect('vault:accountInfo',account_no_pk)
 		else:
 			return HttpResponse("Invalid Amount")
 	
 	if request.method=='GET':
 		return render(request, 'vault/credit.html', context)
 
+
 def login_success(request):
-	if request.user.groups.filter(name="Regular").exists():
-		# user is an admin
+	if request.user.groups.filter(name="Regular").exists():	
 		return redirect('/internal')
-	else:
+	elif request.user.groups.filter(name="Administrator").exists():
+		return redirect('/administrator')
+	elif request.user.groups.filter(name="Manager").exists():
+		return redirect('/manager')
+	elif request.user.groups.filter(name="Ind_user").exists():
 		return redirect('/')
+	elif request.user.groups.filter(name="Organization").exists():
+		return redirect('/')
+	else:
+		return redirect('/login')
